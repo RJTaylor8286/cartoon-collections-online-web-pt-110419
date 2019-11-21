2bc9f94af6e0c455ecbaf40ef1ad46d0c4a07954
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-21 15:07:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,9 @@ planeteer_calls = ["earth", "fire", "wind", "water", "heart"]
 summon_captain_planet.map
 end
 long_planeteer_calls = ["earth", "fire", "wind", "water", "heart"]
-  # Your code here
+  long_planeteer_calls.include
 end
 
 snacks = ["crackers", "gouda", "thyme"]
   cheese_types = ["cheddar", "gouda", "camembert"]
-find_the_cheese(snacks)
+find_the_cheese(snacks).inlcude
